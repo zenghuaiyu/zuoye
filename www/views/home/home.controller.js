d3be02fffdddd87cc56a57d93257c3cf00d69618
曾怀宇 <1592314399@ qq.com>
2017-09-24 21:26:11 +0800

    生意专家APP
    做了欢迎页、登录页面。首页开始一点点

---
+++ www/views/home/home.controller.js
@@ -0,0 +1,10 @@
+/**
+ * Created by Administrator on 2017/9/19.
+ */
+(function () {
+  'use striet';
+  angular.module('starter.controllers')
+    .controller('HomeCtrl',['$scope',function ($scope){
+      
+    }])
+})();
---
+++ www/views/login/login.controller.js
@@ -0,0 +1,33 @@
+/**
+ * Created by Administrator on 2017/9/19.
+ */
+(function () {
+  'use striet';
+  angular.module('starter.controllers')
+    .controller('LoginCtrl', ['$scope', '$ionicPopup', 'localStorageService', '$state',function ($scope, $ionicPopup, localStorageService,$state) {
+      var USER_KEY = 'user';
+      $scope.user = {
+        username: '',
+        password: ''
+      };
+      $scope.login = function () {
+        var account = localStorageService.get(USER_KEY, {
+          username: 'zhy',
+          password: '111222'
+        });
+        if (account.username === $scope.user.username && account.password === $scope.user.password) {
+          account.isLogin = true;
+          localStorageService.update(USER_KEY, account);
+          $state.go('app.home');
+        }
+        else {
+          $ionicPopup.alert({
+            title: '警告',
+            template: '用户或密码不正确',
+            okText: '确定',
+            okType: 'button-energized'
+          });
+        }
+      };
+    }]);
+})();
