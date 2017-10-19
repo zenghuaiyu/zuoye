275553de20fd48c9fd856ea0cf8bc3c9804608c8
曾怀宇 <1592314399@qq.com>
2017-10-19 22:08:33 +0800

    完成作业

---
+++ www/views/setting/setting.controller.js
@@ -0,0 +1,28 @@
+/**
+ * Created by 曾怀宇 on 2017/10/19.
+ */
+
+(function () {
+  'use strict';
+  angular.module('starter.controllers')
+    .controller('SettingCtrl',['$scope','$ionicHistory','$state','localStorageService',function ($scope,$ionicHistory,$state,localStorageService) {
+      $scope.goBack=function () {
+        $ionicHistory.nextViewOptions({
+          disableBack:true
+        });
+        $state.go('app.Home')
+      };
+      $scope.shop={
+        shopPhone:'19971997'
+      };
+      $scope.callPhone=function () {
+        location.href='tel:' + $scope.shop.shopPhone;
+      };
+      $scope.login=function () {
+        var account = localStorageService.get('User')
+        account.isLogin=false;
+        localStorageService.update('User',account);
+        $state.go('app.Home')
+      }
+    }]);
+})();
---
+++ www/views/shop/shop-edit.controller.js
@@ -0,0 +1,22 @@
+/**
+ * Created by 曾怀宇 on 2017/10/19.
+ */
+(function () {
+  'use strict';
+  angular.module('starter.controllers').controller('ShopEditCtrl',['$scope','$stateParams','localStorageService',function ($scope,$stateParams,localStorageService) {
+    $scope.title=$stateParams.title;
+    $scope.property=$stateParams.property;
+    $scope.shop = localStorageService.get('Shop',{
+      phone:'19971997',
+      createTime:'2017-10-01 11:11:11',
+      name:'',
+      ab:'',
+      boss:'',
+      email:'',
+      shopPhone:''
+    });
+    $scope.save=function () {
+      localStorageService.update('Shop',$scope.shop);
+    }
+  }])
+})()
